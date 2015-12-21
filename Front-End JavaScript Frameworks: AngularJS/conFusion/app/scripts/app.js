5325c9a1dd4c32b3de12c8f9376b03d5184381bc
Nicolás Batistoni <nicolas.batistoni@hotmail.com>
2015-12-21 09:00:38 -0300

    Angular 2nd Assignment Done.

--- Front-End JavaScript Frameworks: AngularJS/conFusion/app/scripts/app.js
+++ Front-End JavaScript Frameworks: AngularJS/conFusion/app/scripts/app.js
@@ -5,7 +5,7 @@ angular.module('confusionApp', [])
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            
+
             $scope.dishes=[
                          {
                           name:'Uthapizza',
@@ -66,4 +66,96 @@ angular.module('confusionApp', [])
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+          $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+          var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+          $scope.channels = channels;
+          $scope.invalidChannelSelection = false;
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+          $scope.sendFeedback = function() {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+              $scope.invalidChannelSelection = true;
+              console.log('incorrect');
+            }
+            else {
+              $scope.invalidChannelSelection = false;
+              $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                 agree:false, email:"" };
+              $scope.feedback.mychannel="";
+
+              $scope.feedbackForm.$setPristine();
+              console.log($scope.feedback);
+            }
+          };
+        }])
+
+        .controller('DishDetailController', ['$scope', function($scope) {
+          var dish = {
+                        name:'Uthapizza',
+                        image: 'images/uthapizza.png',
+                        category: 'mains',
+                        label:'Hot',
+                        price:'4.99',
+                        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                         comments: [
+                             {
+                                 rating:5,
+                                 comment:"Imagine all the eatables, living in conFusion!",
+                                 author:"John Lemon",
+                                 date:"2012-10-16T17:57:28.556094Z"
+                             },
+                             {
+                                 rating:4,
+                                 comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                 author:"Paul McVites",
+                                 date:"2014-09-05T17:57:28.556094Z"
+                             },
+                             {
+                                 rating:3,
+                                 comment:"Eat it, just eat it!",
+                                 author:"Michael Jaikishan",
+                                 date:"2015-02-13T17:57:28.556094Z"
+                             },
+                             {
+                                 rating:4,
+                                 comment:"Ultimate, Reaching for the stars!",
+                                 author:"Ringo Starry",
+                                 date:"2013-12-02T17:57:28.556094Z"
+                             },
+                             {
+                                 rating:2,
+                                 comment:"It's your birthday, we're gonna party!",
+                                 author:"25 Cent",
+                                 date:"2011-12-02T17:57:28.556094Z"
+                             }
+
+                         ]
+                  };
+              $scope.dish = dish;
+
+              var newComment = {
+                rating:5,
+                comment:"",
+                author:"",
+                date:""
+              };
+
+              $scope.newComment = newComment;
+
+              $scope.sendComments = function() {
+                $scope.newComment.date = Date.now();
+                $scope.dish.comments.push($scope.newComment);
+                $scope.newComment = {
+                  rating:5,
+                  comment:"",
+                  author:"",
+                  date:""
+                };
+                $scope.dishCommentForm.$setPristine();
+              };
         }]);
