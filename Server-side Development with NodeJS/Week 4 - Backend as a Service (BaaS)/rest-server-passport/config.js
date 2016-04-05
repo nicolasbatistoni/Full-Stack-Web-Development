f3c224cd02910b6c8b7c11fe8eb4af3079585dc0
Nicolás Batistoni <nicolas.batistoni@hotmail.com>
2016-04-05 14:24:06 -0300

    wip

--- Server-side Development with NodeJS/Week 4 - Backend as a Service (BaaS)/rest-server-passport/config.js
+++ Server-side Development with NodeJS/Week 4 - Backend as a Service (BaaS)/rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:7007/conFusion'
+    'mongoUrl' : 'mongodb://localhost:7007/conFusion',
+    'facebook': {
+        clientID: '1717860735102489',
+        clientSecret: 'c8859676e16cdaca5915d116ba0d7b7f',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
